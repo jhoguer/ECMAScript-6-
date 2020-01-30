b76dfac6c325860e2e857e65451eb35f07eda3d4
Jhon Guerrero <jhoguer@gmail.com>
2020-01-30 15:15:22 -0500

    Enteniendo clases, modulos y generators

--- src/es6/index.js
+++ src/es6/index.js
@@ -62,3 +62,80 @@ console.log(globalVar);
 console.log(globalLet);
 
 const a = 'b';
+a = 'a'
+
+let name = 'Jhon';
+let age = 33;
+
+// es5
+obj = { name: name, age: age };
+//es6
+obj2 = { name, age };
+console.log(obj2);
+
+const names = [
+  { name: 'Jhon', age: 33 },
+  { name: 'Yesica', age: 27 }
+]
+
+let listOfNames = names.map(function(item) {
+  console.log(item.name);
+})
+
+let listOfNames2 = names.map(item => console.log(item.name))
+
+const listOfNames3 = (name, age) => {
+  ...
+}
+
+const listOfNames4 = name => {
+  ...
+}
+
+const square = nun => num * num;
+
+const helloPromise = () => {
+  return new Promise((resolve, reject) => {
+    if (false) {
+      resolve('Hey!!!');
+    } else {
+      reject('Ups!!');
+    }
+  })
+}
+
+helloPromise()
+  .then(response => console.log(response))
+  .then(() => console.log('Hola'))
+  .catch(err => console.log(err));
+
+  class calculator {
+    constructor() {
+      this.valueA = 0;
+      this.valueB = 0;
+    }
+
+    sum(valueA, valueB) {
+      this.valueA = valueA;
+      this.valueB = valueB;
+      return this.valueA + this.valueB;
+    }
+  }
+
+  const calc = new calculator();
+  console.log(calc.sum(8, 3));
+
+import {hello} from './module';
+
+hello();
+
+function* helloWorld() {
+  if (true) {
+    yield 'Hello, ';
+  }
+  if (true) {
+    yield 'World';
+  }
+}
+
+const generatorHello = helloWorld();
